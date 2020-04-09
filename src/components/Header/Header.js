3fb95485088433f209cf677888c6668b96c6434f
Michał Świderski <michal.sw12@gmail.com>
2020-04-09 23:36:25 +0200

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.png';
+
+
+const Header = () => {
+    return (
+        <div>
+            <img src={logo} alt="logo"/>
+        </div>
+    );
+};
+
+export default Header;
