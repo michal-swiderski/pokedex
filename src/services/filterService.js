a5f00b607dc93da55136d8993796a07cf46b90b2
Michał Świderski <michal.sw12@gmail.com>
2020-04-11 14:56:04 +0200

    add filtering

---
+++ src/services/filterService.js
@@ -0,0 +1,33 @@
+import {fetchPokemonByName, fetchPokemonList, fetchPokemonListByType} from "./pokemonService";
+import {intersectionBy} from 'lodash';
+
+export async function getFilteredPokemons(name = '', types = [], limit = 20, offset = 0) {
+    try {
+        const pokemonNamesList = await fetchPokemonList();
+        const filteredByName = pokemonNamesList.filter(pokemon => pokemon.name.startsWith(name));
+
+        const pokemonsByType = [];
+        const typeRequests = [];
+        types.forEach(type => {
+            const req = fetchPokemonListByType(type).then(pokemons => {
+                pokemonsByType.push(pokemons)
+            });
+            typeRequests.push(req);
+        });
+
+        await Promise.all(typeRequests);
+        let filtered = intersectionBy(filteredByName, ...pokemonsByType, 'name');
+        const count = filtered.length;
+        filtered = filtered.slice(offset, offset + limit);
+
+        const filteredResults = [];
+        await Promise.all(filtered.map(({name}) => fetchPokemonByName(name).then(pokemon => filteredResults.push(pokemon))));
+
+        return {
+            totalCount: count,
+            results: filteredResults.sort((a, b) => a.id - b.id)
+        };
+    } catch (error) {
+        throw error;
+    }
+}
