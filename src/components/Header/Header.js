dac21fd2d27e0496adb722ea172dc29dd6753333
Michał Świderski <michal.sw12@gmail.com>
2020-04-13 01:03:04 +0200

    make header smaller

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Header.scss';
 import logo from '../../assets/logo.png';
 
 
 const Header = () => {
     return (
         <header className="header">
-            <img src={logo} alt="logo"/>
+            <img className="header__image" src={logo} alt="logo"/>
         </header>
     );
 };
