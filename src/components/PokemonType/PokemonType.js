5c8e5e6eec3e84e81d520b6c4a137f4d16d848a8
Michał Świderski <michal.sw12@gmail.com>
2020-04-13 00:49:00 +0200

    make type chips colors css based

--- src/components/PokemonType/PokemonType.js
+++ src/components/PokemonType/PokemonType.js
@@ -2,30 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './PokemonType.scss';
 
-//TODO add the rest
-const types = {
-    water: '#6890F0',
-    poison: '#A040A0',
-    grass: '#78C850',
-    fire: '#F08030',
-    flying: '#A890F0',
-    bug: '#A8B820',
-    normal: '#A8A878'
-};
-
 const PokemonType = props => {
 
-    const color = types[props.type];
-
     return (
-        <div className="pokemon-type" style={{color, borderColor: color}}>
+        <div className={"pokemon-type pokemon-type--" + props.type}>
             {props.type}
         </div>
     );
 };
 
 PokemonType.propTypes = {
-    type: PropTypes.string
+    type: PropTypes.string.isRequired
 };
 
 export default PokemonType;
