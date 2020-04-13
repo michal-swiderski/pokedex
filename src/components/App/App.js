475a234578aa5dbd2b237c51672b1f7750c11098
Michał Świderski <michal.sw12@gmail.com>
2020-04-13 19:14:17 +0200

    redirect to first page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
-import {Route, BrowserRouter, Switch} from 'react-router-dom';
+import {Route, BrowserRouter, Switch, Redirect} from 'react-router-dom';
 
 import Header from "../Header/Header";
 import PokemonList from "../PokemonList/PokemonList";
+import PokemonDetails from "../PokemonDetails/PokemonDetails";
 
 function App() {
     return (
@@ -11,10 +12,14 @@ function App() {
             <div className="App">
                 <Header/>
                 <Switch>
-                    <Route path="/:page?">
+                    <Redirect exact from='/' to='/1'/>
+                    <Route exact path="/:page">
                         <div className="container">
                             <PokemonList/>
                         </div>
+                    </Route>
+                    <Route path="/pokemon/:name">
+                        <PokemonDetails/>
                     </Route>
                 </Switch>
             </div>
