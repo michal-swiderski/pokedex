21deee218610d6bb9ec13e4479bf13d93bf2aec9
Michał Świderski <michal.sw12@gmail.com>
2020-04-14 01:36:02 +0200

    add basic navigation

--- src/components/PokemonDetails/PokemonDetails.js
+++ src/components/PokemonDetails/PokemonDetails.js
@@ -1,23 +1,34 @@
 import React, {useState, useEffect} from 'react';
-import {useParams} from 'react-router-dom';
+import {useParams, useHistory} from 'react-router-dom';
 import './PokemonDetails.scss';
-import {fetchPokemonByName, fetchPokemonSpeciesByName} from "../../services/pokemonService";
+import {fetchPokemonByNameOrId, fetchPokemonSpeciesByName} from "../../services/pokemonService";
 import PokemonType from "../PokemonType/PokemonType";
 import StatBar from "../StatBar/StatBar";
+import {sample} from 'lodash';
+
+import arrow_back from '../../assets/icons/arrow_back.svg';
+import arrow_forward from '../../assets/icons/arrow_forward.svg';
+
 
 const PokemonDetails = () => {
 
     const [pokemon, setPokemon] = useState(null);
     const [pokemonSpecies, setPokemonSpecies] = useState(null);
+    const [flavorText, setFlavorText] = useState(null);
 
+    const history = useHistory();
     const {name} = useParams();
 
+    useEffect(() => console.log('rerender'))
+
     useEffect(() => {
-        fetchPokemonByName(name).then(pokemon => {
+        fetchPokemonByNameOrId(name).then(pokemon => {
             setPokemon(pokemon);
             return fetchPokemonSpeciesByName(pokemon.species.name);
         }).then(species => {
             setPokemonSpecies(species);
+            const flavor_texts = species.flavor_text_entries.filter(el => el.language.name === 'en').map(el => el.flavor_text);
+            setFlavorText(sample(flavor_texts));
         }).catch(e => {
             //TODO proper error handling
         });
@@ -28,11 +39,21 @@ const PokemonDetails = () => {
         return null;
     }
 
-    const desription = pokemonSpecies.flavor_text_entries.find(el => el.language.name === 'en').flavor_text;
-
     return (
         <div className="pokemon-details">
             <div className="pokemon-details__image-wrapper">
+                <button className="pokemon-details__nav-button pokemon-details__nav-button--back"
+                        onClick={() => history.push('/pokemon/' + (pokemon.id - 1))}>
+                    <img src={arrow_back} alt="arrow left"/>
+                    <span>#{(pokemon.id - 1).toString().padStart(3, '0')}</span>
+                </button>
+
+                <button className="pokemon-details__nav-button pokemon-details__nav-button--forward"
+                        onClick={() => history.push('/pokemon/' + (pokemon.id + 1))}>
+                    <span>#{(pokemon.id + 1).toString().padStart(3, '0')}</span>
+                    <img src={arrow_forward} alt="arrow right"/>
+                </button>
+
                 <img className="pokemon-details__image" src={pokemon.sprites.front_default} alt={pokemon.name}/>
             </div>
             <h1 className="pokemon-details__name">{pokemon.name}</h1>
@@ -40,7 +61,7 @@ const PokemonDetails = () => {
                 <span className="pokemon-details__size">Weight: {pokemon.weight / 10} kg</span>
                 <span className="pokemon-details__size">Height: {pokemon.height / 10} m</span>
             </div>
-            <p className="pokemon-details__description">"{desription}"</p>
+            <p className="pokemon-details__description">"{flavorText}"</p>
             <h2>Types</h2>
             {pokemon.types.map(({type}) => <PokemonType key={type.name} type={type.name}/>)}
             <h2>Stats</h2>
