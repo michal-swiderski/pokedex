f31ec2066b36b53344836bd3519bdb25bc1e6719
Michał Świderski <michal.sw12@gmail.com>
2020-04-14 02:32:49 +0200

    add full width option to type chip

--- src/components/PokemonType/PokemonType.js
+++ src/components/PokemonType/PokemonType.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './PokemonType.scss';
+import clsx from "clsx";
 
 const PokemonType = props => {
 
     return (
         <div
-            className={"pokemon-type pokemon-type--" + props.type}>
+            className={clsx("pokemon-type pokemon-type--" + props.type, {"pokemon-type--full-width": props.fullWidth})}>
             {props.type}
         </div>
     );
