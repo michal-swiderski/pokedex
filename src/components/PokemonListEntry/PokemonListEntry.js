6abe7f2ddb9d9035f7a8972fb2cd4f6d86381925
Michał Świderski <michal.sw12@gmail.com>
2020-04-14 02:34:47 +0200

    redirect on click

--- src/components/PokemonListEntry/PokemonListEntry.js
+++ src/components/PokemonListEntry/PokemonListEntry.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 import './PokemonListEntry.scss';
 import PropTypes from 'prop-types';
 import PokemonType from "../PokemonType/PokemonType";
 
 const PokemonListEntry = props => {
+
+    const history = useHistory();
+
     return (
-        <div className="pokemon-list-entry">
+        <div className="pokemon-list-entry" onClick={() => history.push('/pokemon/' + props.pokemon.name)}>
             <div>
                 <div>
                     <span className="pokemon-list-entry__id">#{props.pokemon.id.toString().padStart(3, '0')}</span>
                     <h2 className="pokemon-list-entry__name">{props.pokemon.name}</h2>
                 </div>
                 {
-                    props.pokemon.types.map(type => <PokemonType type={type.type.name} fullWidth key={type.type.name}/>)
+                    props.pokemon.types.map(type => <PokemonType type={type.type.name} key={type.type.name}/>)
                 }
             </div>
             <div className="pokemon-list-entry__image-wrapper">
