5988fe78cac3991b1d4432a7dd74c8e9e599860c
Michał Świderski <michal.sw12@gmail.com>
2020-04-16 00:31:15 +0200

    add a footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,37 +1,41 @@
 import React from 'react';
 import './App.css';
-import {Route, BrowserRouter, Switch, Redirect} from 'react-router-dom';
+import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom';
 
 import Header from "../Header/Header";
 import PokemonList from "../PokemonList/PokemonList";
 import PokemonDetails from "../PokemonDetails/PokemonDetails";
 import ErrorPage from "../ErrorPage/ErrorPage";
+import Footer from "../Footer/Footer";
 
 function App() {
     return (
-        <BrowserRouter>
-            <div className="App">
+        <div className="app">
+            <BrowserRouter>
                 <Header/>
-                <Switch>
-                    <Redirect exact from='/' to='/page/1'/>
-                    <Route exact path="/page/:page">
-                        <div className="container">
-                            <PokemonList/>
-                        </div>
-                    </Route>
-                    <Route path="/pokemon/:name">
-                        <PokemonDetails/>
-                    </Route>
-                    <Route path="/error">
-                        <ErrorPage msg="A fatal error has occured."/>
-                    </Route>
-                    <Route path="/404">
-                        <ErrorPage msg="Page not found."/>
-                    </Route>
-                    <Redirect to="/404"/>
-                </Switch>
-            </div>
-        </BrowserRouter>
+                <div className="content-wrapper">
+                    <Switch>
+                        <Redirect exact from='/' to='/page/1'/>
+                        <Route exact path="/page/:page">
+                            <div className="container">
+                                <PokemonList/>
+                            </div>
+                        </Route>
+                        <Route path="/pokemon/:name">
+                            <PokemonDetails/>
+                        </Route>
+                        <Route path="/error">
+                            <ErrorPage msg="A fatal error has occurred."/>
+                        </Route>
+                        <Route path="/404">
+                            <ErrorPage msg="Page not found."/>
+                        </Route>
+                        <Redirect to="/404"/>
+                    </Switch>
+                </div>
+                <Footer/>
+            </BrowserRouter>
+        </div>
     );
 }
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <a href="https://github.com/michal-swiderski">Michał Świderski</a> 2020
+        </footer>
+    );
+};
+
+export default Footer;
