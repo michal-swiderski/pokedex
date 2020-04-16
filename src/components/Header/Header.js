fc248aad6ea7c8cdc1ebff077ac1e123b3932a83
Michał Świderski <michal.sw12@gmail.com>
2020-04-16 16:41:03 +0200

    make logo a link to first page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Header.scss';
+import {Link} from "react-router-dom";
 import logo from '../../assets/logo.png';
 
 
 const Header = () => {
     return (
         <header className="header">
-            <img className="header__image" src={logo} alt="logo"/>
+            <Link to="/page/1"><img className="header__image" src={logo} alt="logo"/></Link>
         </header>
     );
 };
